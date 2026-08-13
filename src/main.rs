19f7cf0b9c7b2f1f8f7f69e40d77fa0e493eaaa9
Ryba37 <pidchenkomatvii@gmail.com>
2026-08-13 20:07:17 +0200

    fuck wgpu

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
-use winit::event_loop::{ControlFlow, EventLoop};
-
-mod app;
-mod gpu;
-mod text;
-
 fn main() {
-    let event_loop = EventLoop::new().unwrap();
-
-    // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
-    // dispatched any events. This is ideal for games and similar applications.
-    event_loop.set_control_flow(ControlFlow::Poll);
-
-    // ControlFlow::Wait pauses the event loop if no events are available to process.
-    // This is ideal for non-game applications that only update in response to user
-    // input, and uses significantly less power/CPU time than ControlFlow::Poll.
-
-    let mut app = app::App::new();
-    let _ = event_loop.run_app(&mut app);
+    todo!()
 }
